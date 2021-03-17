41fb8e487a6751cd4295ae24a4d0ef14eaaea7e8
Pete Turnham <peteturnham@gmail.com>
2021-03-17 17:01:16 -0400

    jupyter notebook initial upload

---
+++ mision_to_Mars/run_this_app.py
@@ -0,0 +1,12 @@
+# flask app 
+import nump as np 
+from flask import Flask
+import requests
+import pymongo
+
+
+
+###################################
+# data base set up
+###################################
+
