3e930113d461280950bd7b315c9f8a079ef22de9
Pete Turnham <peteturnham@gmail.com>
2021-03-19 13:24:19 -0400

    added index.html, scrape.py

--- mision_to_Mars/run_this_app.py
+++ mision_to_Mars/run_this_app.py
@@ -1,37 +1,40 @@
 ########################
 # flask app
 ########################
- 
-import nump as np 
-from flask import Flask
-import requests
-import pymongo
-
-
-
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape
 ###################################
 # data base set up
 ###################################
 # Flask set up
 app = Flask(__name__)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 
 
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
 
+    # Find one record of data from the mongo database
+    data = mongo.db.mars_data.find_one()
 
-
-
+    # Return template and data
+    return render_template("index.html",data = data )
 
 ###################################
 # data scraping
 ###################################
 @app.route("/scrape")
+def scrape():
+    #run the scrape function
+    mars_data= scrape.scrape()
+
+    mongo.db.collection.update({},mars_data, upsert=True)
+    #return to home page
+    return redirect("/", code=302)
 
 
-
-    
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- mision_to_Mars/scrape.py
+++ mision_to_Mars/scrape.py
@@ -1,17 +1,16 @@
-
-# 3/17/21
-############ NEEDS TO BE MODIFED TO INTERACT WITH INDEX, DATABASE, AND APP SCIPT
-
-#dependencieS
-import splitner 
-import webdriver_manager.chrome import ChromeDriverManager
-import import BeautifulSoup as bs
+#dependencies
+import pandas as pd
+import splinter 
+from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
-#web scraping 
-def news_scrape():
-    #setting up browser connection and path
+
+def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    return Browser('chrome', **executable_path, headless=False)
+
+#web scraping 
+def scrape():
+#############################################################
     #link as variable
     url = 'https://mars.nasa.gov/news/'
     #commencing website connection
@@ -25,15 +24,101 @@ def news_scrape():
         #storing relevant data in variables
         header = result.find('div', class_= 'content_title')
         paragraph = result.find('div', class_='article_teaser_body')
-        #printing variables to ensure we have retrieved them
-        print('------------------------------------------------------------------')
-        print(header.text)
-        print(paragraph.text)
-        print('------------------------------------------------------------------')
-        #storing results in variable to upload to MongoDB
-        post = {
-        'header': header,
-        'paragraph': paragraph
-                }
-        #qutting browser session
+        #quit browser
+        browser.quit()        
+#############################################################
+    #scarping data for featured image
+    #url as variable
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    #commencing website connection
+    browser.visit(url)
+    # scrap data into soup
+    html = browser.html
+    soup = bs(html,'html.parser')
+
+    urls=[]
+    images = soup.find('a',class_='showimg fancybox-thumbs')
+    href = images['href']
+
+    path = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/' +href
+    #quit browser
+    browser.quit()
+    #storing path as variable to push to mongoDB
+    featured_post = path
+    # mars.db.insert_one(featured_post)
+#############################################################
+# scraping data to create html table
+    url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url)
+    html_table = df.to_html()
+    html_mars_table=html_table.replace('\n','')
+    # mars.db.insert_one(html_mars_table)
+#############################################################
+# scraping news site for hi res images for each hemisphere and their url's
+    browser.visit('https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
+    #limit calls to 1 every minute
+    time.sleep(1)
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    #hemisphere name variable
+    hemi_names = []
+    # Search for the names of all four hemispheres
+    results = soup.find_all('div', class_="collapsible results")
+    #target hemisphere names
+    hemispheres = results[0].find_all('h3')
+    # Get text and store in list
+    for name in hemispheres:
+        hemi_names.append(name.text)
+    # Search for thumbnail links
+    thumbnail_results = results[0].find_all('a')
+    thumbnail_links = []
+    # Iterate through thumbnail links for full-size image
+    for thumbnail in thumbnail_results:
+        # If the thumbnail element has an image...
+        if (thumbnail.img):
+            # then grab the attached link
+            thumbnail_url = 'https://astrogeology.usgs.gov/' + thumbnail['href']
+            # Append list with links
+            thumbnail_links.append(thumbnail_url)
+    full_imgs = []
+    #itereate through 
+    for url in thumbnail_links:
+        # Click through each thumbanil link
+        browser.visit(url)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        # Scrape each page for the relative image path
+        results = soup.find_all('img', class_='wide-image')
+        relative_img_path = results[0]['src']
+        # Combine the reltaive image path to get the full url
+        img_link = 'https://astrogeology.usgs.gov/' + relative_img_path
+        # Add full image links to a list
+        full_imgs.append(img_link)
+    # Zip together the list of hemisphere names and hemisphere image links
+    mars_hemi_zip = zip(hemi_names, full_imgs)
+    #hemiphsere img url varibale
+    hemisphere_image_urls = []
+    # Iterate through the zipped object
+    for title, img in mars_hemi_zip:
+        #dictionary variable for all data
+        mars_hemi_dict = {}
+        # Add hemisphere title to dictionary
+        mars_hemi_dict['title'] = title
+        # Add image url to dictionary
+        mars_hemi_dict['img_url'] = img
+        # Append the list with dictionaries
+        hemisphere_image_urls.append(mars_hemi_dict)
+        #quit browser
         browser.quit()
+        # db.mars_data.insert(mars_hemi_dict)
+
+#############################################################
+# uploading data to mongoDB 
+    mars_data = {}
+
+    mars_data['header']= header
+    mars_data['paragraph']=paragraph
+    mars_data['featured image']=path
+    mars_data['hemisphere images']= mars_hemi_dict
+    return mars_data 
